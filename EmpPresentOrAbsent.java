01d572d0a2860dc84a01769a8b421ef216a67ac5
aquib98 <aquib.syed1997@gmail.com>
2023-04-08 11:27:17 -0700

    Added Employee Present or Absent  Files

---
+++ EmpPresentOrAbsent.java
@@ -0,0 +1,23 @@
+package WageSalary;
+ 
+
+public class EmpPresentOrAbsent {
+	 
+	public static void main(String[] args) {
+		 System.out.println("Welcome to Employee Wage computation");
+		 
+		/*
+		 * 1) check weather the employee is present or absent 
+        */
+		
+		 double isPresent = Math.random();
+		 if (isPresent <=0.5) {
+			 System.out.println("employee is present");
+		 }
+		 else {
+			 System.out.println("employee is absent");
+				 
+		 }
+	}
+
+}
