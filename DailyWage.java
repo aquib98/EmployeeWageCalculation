0bfa15086e3697fc911017dbaca787f02a22a8fa
aquib98 <aquib.syed1997@gmail.com>
2023-04-08 11:32:10 -0700

    Added Daily Wage   File

---
+++ DailyWage.java
@@ -0,0 +1,24 @@
+package WageSalary;
+
+public class DailyWage {
+
+	public static void main(String[] args) {
+		 /*
+		  * Initiating EmpWage  and Hours of work variables
+		  * Assigning the assumed values to the integer variables
+		  */
+		 System.out.println("Welcome to Employee Wage computation");
+		 int Empwage , hours ;
+		 Empwage = 20 ;
+		 hours = 8 ;
+		 /*
+		  * calculating the dailyWage of the employee
+		  */
+		 int  dailyWage = Empwage*hours ;
+		 System.out.println("The Dailywage of the Employee is : " +dailyWage);
+		 
+		 
+		 
+	}
+
+}
